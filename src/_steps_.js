840ffb875068944400bb821c8eea0266f059a814
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-11 01:21:56 +0600

    context setup

---
+++ src/_steps_.js
@@ -0,0 +1,12 @@
+/**
+ * CONTEXT API
+ * 1. Create Context (export)
+ * 2. Create Provider
+ * 3. Set context value
+ * 4. to access the value: useContext hook
+ */
+
+/**
+ * 1. main.jsx set AuthProvider
+ * 2. inside provider access children props and then use
+ */
