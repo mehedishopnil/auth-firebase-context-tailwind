74a19675efd2285fd2a10bb7723387810d5668c6
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-12 01:31:59 +0600

    context login and registration

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,12 +1,29 @@
-import React, { createContext } from 'react';
+import React, { createContext, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
+const auth = getAuth(app);
 
- const AuthProviders = ({children}) => {
+const AuthProviders = ({children}) => {
 
-    const user = {displayName: 'Sagor Nodi'}
+  const [user, setUser] = useState(null); 
+  
+  const createUser = (email, password) =>{
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  const signIn = ( email, password) =>{
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+    const authInfo = {
+        user,
+        createUser,
+        signIn
+    }
     return (
-        <AuthContext.Provider value={user}>
+        <AuthContext.Provider value={authInfo}>
             {children}
         </AuthContext.Provider>
     );
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProviders';
 
 const Register = () => {
+
+    const {user, createUser} = useContext(AuthContext);
+    console.log(createUser);
 
     const handleRegister = event => {
         event.preventDefault();
@@ -10,6 +14,16 @@ const Register = () => {
         const email = form.email.value;
         const password = form.password.value;
         console.log(name, email, password);
+
+        createUser(email, password)
+        .then(result =>{
+            const loggedUser = result.user;
+            console.log(loggedUser);
+            form.reset();
+        })
+        .catch (error =>{
+            console.log(error);
+        })
     }
     return (
         <div className="hero min-h-screen bg-[#f4f3f3]">
@@ -42,7 +56,7 @@ const Register = () => {
                         </div>
 
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Register</button>
                         </div>
                     </form>
                     <Link to ='/login'>
