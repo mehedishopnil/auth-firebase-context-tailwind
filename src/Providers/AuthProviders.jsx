dd799362a715a45543598e2d0956103378776a5d
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2023-06-12 10:42:18 +0600

    sign in and sign out cunctionality

--- src/Providers/AuthProviders.jsx
+++ src/Providers/AuthProviders.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext(null);
@@ -17,11 +17,28 @@ const AuthProviders = ({children}) => {
     return signInWithEmailAndPassword(auth, email, password);
   }
 
+  const logOut = () =>{
+    return signOut(auth);
+  }
+
+  // observe auth state change
+  useEffect( () => {
+   const unsubscribe = onAuthStateChanged(auth, currentUser => {
+      console.log('Auth state change', currentUser);
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    }
+  }, [])
+
     const authInfo = {
         user,
         createUser,
-        signIn
-    }
+        signIn,
+        logOut
+      }
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
